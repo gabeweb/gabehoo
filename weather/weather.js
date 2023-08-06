b8b26f03a1045c55fcd754238e28463ac3a2152a
Gabe Pérez <1930100+gabeweb@users.noreply.github.com>
2023-08-05 22:19:36 -0400

    Add files via upload

--- weather/weather.js
+++ weather/weather.js
@@ -1,13 +1,10 @@
-// Weather
-// Weather icons by Icons8
-// https://icons8.com/icon/set/weather/color-glass
-App = {
-  getWeatherIcon: function (iconCode) {
-    var dayNight = iconCode.slice(2, 3);
-    console.log(dayNight);
-    var code = iconCode.slice(0, 2);
-    if (dayNight == "d") {
-      switch (code) {
+(App = {
+  getWeatherIcon: function (e) {
+    var t = e.slice(2, 3);
+    console.log(t);
+    var n = e.slice(0, 2);
+    if ("d" == t)
+      switch (n) {
         case "01":
           return "weather/icons/sunny.svg";
         case "02":
@@ -29,8 +26,8 @@ App = {
         default:
           return null;
       }
-    } else {
-      switch (code) {
+    else
+      switch (n) {
         case "01":
           return "weather/icons/night.svg";
         case "02":
@@ -52,56 +49,31 @@ App = {
         default:
           return null;
       }
-    }
   },
-
   getWeather: function () {
-    let xhr = new XMLHttpRequest();
-    /* OPEN WEATHER MAP */
-    xhr.open(
+    let e = new XMLHttpRequest();
+    e.open(
       "GET",
       "https://api.openweathermap.org/data/2.5/weather?q=Cabimas,Venezuela&appid=dba306998ebe631bfd796148d0ff7a9b&units=metric"
-    );
-    xhr.onload = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          let json = JSON.parse(xhr.responseText);
-          var temp = json.main.feels_like.toFixed(0) + "&deg;C";
-          var weatherDescription = json.weather[0].description;
-          var weatherIcon = App.getWeatherIcon(json.weather[0].icon);
-          if (weatherIcon) {
-            document.getElementById("weather-container").title =
-              weatherDescription;
-            document.getElementById("weather").innerHTML = temp;
-            document.getElementById("weather-icon").src = weatherIcon;
-          } else {
-            document.getElementById("weather").innerHTML =
-              weatherDescription + temp;
-          }
-        } else {
-          console.log("error msg: " + xhr.status);
-        }
-      }
-    };
-
-    /* Aus BOM */
-    //xhr.open('GET', "https://api.weather.bom.gov.au/v1/locations/r3gx2f/observations");
-    //xhr.onload = () => {
-    //  if (xhr.readyState === 4) {
-    //    if (xhr.status === 200) {
-    //      let json = JSON.parse(xhr.responseText);
-    //      //var temp = json.main.temp.toFixed(0) + "&deg;C";
-    //    } else {
-    //      console.log('error msg: ' + xhr.status);
-    //    }
-    //}
-    xhr.send();
+    ),
+      (e.onload = () => {
+        if (4 === e.readyState)
+          if (200 === e.status) {
+            let s = JSON.parse(e.responseText);
+            var t = s.main.feels_like.toFixed(0) + "&deg;C",
+              n = s.weather[0].description,
+              r = App.getWeatherIcon(s.weather[0].icon);
+            r
+              ? ((document.getElementById("weather-container").title = n),
+                (document.getElementById("weather").innerHTML = t),
+                (document.getElementById("weather-icon").src = r))
+              : (document.getElementById("weather").innerHTML = n + t);
+          } else console.log("error msg: " + e.status);
+      }),
+      e.send();
   },
-
   init: function () {
-    /* Weather */
     App.getWeather();
   },
-};
-
-window.onload = App.init();
+}),
+  (window.onload = App.init());
